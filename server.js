a45568a816e9520d994691c00f7eb6e61f7b8748
mesa1014 <mehrdad.sangi@gmail.com>
2021-08-19 11:01:36 +1200

    removed compression

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const compression = require('compression');
+// const compression = require('compression');
 const express = require("express");
 const app = express();
 
-app.use(compression());
+// app.use(compression());
 app.use(express.static('public'))
 
 // app.get("/", function (req, res) {
