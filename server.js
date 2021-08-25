f3d2707bcc03a1acebaca6d87e32c9c9bd7d055d
mesa1014 <mehrdad.sangi@gmail.com>
2021-08-26 10:10:15 +1200

    update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const compression = require("compression");
+const compression = require('compression');
 const express = require("express");
 const app = express();
 
 app.use(compression());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // app.get("/", function (req, res) {
 //     res.sendFile(__dirname + "/index.html");
